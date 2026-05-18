60c6c9b461858ab739a2b625e2432c36d5f12f5b
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2026-05-18 22:38:23 +0700

    fix: update database credentials to use environment variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -13,19 +13,10 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-<<<<<<< HEAD
     host: process.env.DB_HOST ?? "",
     port: Number(process.env.DB_PORT ?? 5432),
     user: process.env.DB_USER ?? "",
     password: process.env.DB_PASSWORD ?? "",
     database: process.env.DB_NAME ?? "",
-=======
-    host: "localhost",
-    port: 5433,
-    user: "postgres",
-    password: "postgres",
-    database: "idamantsl_db",
-    ssl: false,
->>>>>>> origin/ojan
   },
 });
