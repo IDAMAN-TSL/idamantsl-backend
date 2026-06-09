9486a79f777a026934904c21cb99efc850c71583
ojan <muhammadfauzan292004@gmail.com>
2026-06-09 21:47:28 +0700

    cleanup index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import cors from "cors";
 import authRoutes from "./src/routes/auth.routes";
 import penangkaranRoutes from "./src/routes/penangkaran.routes";
 import pengedaranDnRoutes from "./src/routes/pengedaran-dn.routes";
