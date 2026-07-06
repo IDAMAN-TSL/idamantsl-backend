167aee189f2827fd745a2ff842e8cf6f01b7b6d5
rahmahaisyah <rahmahasyh@gmail.com>
2026-07-06 09:18:20 +0700

    chore: hapus komentar di jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,3 @@
-// Sembunyikan console.error dan console.warn saat testing agar output terminal bersih
-// Hal ini karena banyak test yang mensimulasikan error (seperti return 500) yang memang mencetak error ke console.
 beforeAll(() => {
   jest.spyOn(console, 'error').mockImplementation(() => {});
   jest.spyOn(console, 'warn').mockImplementation(() => {});
@@ -10,8 +8,6 @@ afterAll(() => {
   (console.warn as jest.Mock).mockRestore();
 });
 
-// Mock service Google Sheets secara global agar tidak melakukan hit ke API saat testing
-// dan mencegah error parsing dari library 'ky' (ESM)
 jest.mock('./src/services/google-sheets.service', () => ({
   appendLogToSheet: jest.fn().mockResolvedValue(true)
 }));
